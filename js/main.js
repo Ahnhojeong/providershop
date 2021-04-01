a85f201d7b1198a4207664f393f66ecfad11c17f
Ahnhojeong <77098852+Ahnhojeong@users.noreply.github.com>
2021-04-01 10:39:15 +0900

    provider-v8
    js 파일 경로 수정

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-$((function(){var s=$(".headChange");$(window).scroll((function(){$(this).scrollTop()>$(window).height()?s.addClass("sticky"):s.removeClass("sticky")})),$(".burger").on("click",(function(s){s.preventDefault(),$(".menuwrap, .burger").toggleClass("active")}));var o,i,l,e,t=0,n=0;$(window).on("scroll",(function(){t=$(this).scrollTop(),i=t>n?"down":"up",n=t,clearTimeout(o),o=setTimeout(function(s){"down"==s?(l=$(window).height(),e=$(this).scrollTop(),$(".stamp, .sushi-img, .ramen, .journal-text, .sushi").each((function(s){$(".stamp, .sushi-img, .ramen, .journal-text, .sushi").eq(s).offset().top-l<e&&$(".stamp, .sushi-img, .ramen, .journal-text, .sushi").eq(s).addClass("scroll")}))):($(".stamp").removeClass("scroll"),$(".sushi-img").removeClass("scroll"),$(".ramen").removeClass("scroll"),$(".journal-text").removeClass("scroll"),$(".sushi").removeClass("scroll"))}(i))})),$(".gallery-wrap-container").slick({dots:!1,infinite:!1,slidesToShow:4,slidesToScroll:1,responsive:[{breakpoint:1024,settings:{slidesToShow:3,slidesToScroll:3,infinite:!0,dots:!0}},{breakpoint:600,settings:{slidesToShow:2,slidesToScroll:2}},{breakpoint:480,settings:{slidesToShow:1,slidesToScroll:1}}]}),$(".w-gallery div").slick({slidesToShow:1,slidesToScroll:1,autoplay:!1,variableWidth:!0}),$(".insta-slide").slick({dots:!1,infinite:!1,slidesToShow:4,slidesToScroll:1,responsive:[{breakpoint:1024,settings:{slidesToShow:3,slidesToScroll:3,infinite:!0,dots:!0}},{breakpoint:600,settings:{slidesToShow:2,slidesToScroll:2}},{breakpoint:480,settings:{slidesToShow:1,slidesToScroll:1}}]})}));
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/***/ (() => {
+
+eval("$(function () {\n  //header event\n  var $header = $('.headChange');\n  $(window).scroll(function () {\n    if ($(this).scrollTop() > $(window).height()) {\n      $header.addClass('sticky');\n    } else {\n      $header.removeClass('sticky');\n    }\n  }); //menu\n\n  $('.burger').on('click', function (e) {\n    e.preventDefault();\n    $('.menuwrap, .burger').toggleClass('active');\n  }); //index page - item scroll event\n\n  var first = 0,\n      last = 0,\n      loop,\n      state;\n  var winHei, scrPos, imgPos;\n  $(window).on('scroll', function () {\n    first = $(this).scrollTop(); //스크롤 up down 상태 파악\n\n    if (first > last) {\n      //console.log('down');\n      state = 'down';\n    } else {\n      //console.log('up');\n      state = 'up';\n    }\n\n    last = first;\n    clearTimeout(loop);\n    loop = setTimeout(scrFun(state));\n  }); //스크롤 up down 할일\n\n  function scrFun(updown) {\n    if (updown == 'down') {\n      winHei = $(window).height();\n      scrPos = $(this).scrollTop();\n      $('.stamp, .sushi-img, .ramen, .journal-text, .sushi').each(function (i) {\n        imgPos = $('.stamp, .sushi-img, .ramen, .journal-text, .sushi').eq(i).offset().top;\n\n        if (imgPos - winHei < scrPos) {\n          $('.stamp, .sushi-img, .ramen, .journal-text, .sushi').eq(i).addClass('scroll');\n        }\n      });\n    } else {\n      $('.stamp').removeClass('scroll');\n      $('.sushi-img').removeClass('scroll');\n      $('.ramen').removeClass('scroll');\n      $('.journal-text').removeClass('scroll');\n      $('.sushi').removeClass('scroll');\n    }\n  } //drag event - slick\n\n\n  $('.gallery-wrap-container').slick({\n    dots: false,\n    infinite: false,\n    slidesToShow: 4,\n    slidesToScroll: 1,\n    responsive: [{\n      breakpoint: 1024,\n      settings: {\n        slidesToShow: 3,\n        slidesToScroll: 3,\n        infinite: true,\n        dots: true\n      }\n    }, {\n      breakpoint: 600,\n      settings: {\n        slidesToShow: 2,\n        slidesToScroll: 2\n      }\n    }, {\n      breakpoint: 480,\n      settings: {\n        slidesToShow: 1,\n        slidesToScroll: 1\n      }\n    }]\n  });\n  $('.w-gallery div').slick({\n    slidesToShow: 1,\n    slidesToScroll: 1,\n    autoplay: false,\n    variableWidth: true\n  });\n  $('.insta-slide').slick({\n    dots: false,\n    infinite: false,\n    slidesToShow: 4,\n    slidesToScroll: 1,\n    responsive: [{\n      breakpoint: 1024,\n      settings: {\n        slidesToShow: 3,\n        slidesToScroll: 3,\n        infinite: true,\n        dots: true\n      }\n    }, {\n      breakpoint: 600,\n      settings: {\n        slidesToShow: 2,\n        slidesToScroll: 2\n      }\n    }, {\n      breakpoint: 480,\n      settings: {\n        slidesToShow: 1,\n        slidesToScroll: 1\n      }\n    }]\n  });\n});\n\n//# sourceURL=webpack://providershop-master/./src/js/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/main.js"]();
+/******/ 	
+/******/ })()
+;
--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-window.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector(".cursor"),t=document.querySelectorAll("[href]");console.log(t),window.addEventListener("mousemove",(function(t){e.style="transition:.15s; left:".concat(t.clientX,"px; top:").concat(t.clientY,"px")}));for(var n=0;n<t.length;n++)t[n].addEventListener("mouseenter",o),t[n].addEventListener("mouseleave",o);function o(t){"mouseenter"==t.type?e.classList.add("active"):e.classList.remove("active")}var r=document.querySelector(".circle-pattern");window.addEventListener("scroll",(function(){r.style.transform="translateY("+this.scrollY+"px)"}))}));
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/script.js":
+/*!**************************!*\
+  !*** ./src/js/script.js ***!
+  \**************************/
+/***/ (() => {
+
+eval("window.addEventListener('DOMContentLoaded', function () {\n  var cursor = document.querySelector('.cursor');\n  var linkBtn = document.querySelectorAll('[href]');\n  console.log(linkBtn);\n  window.addEventListener('mousemove', function (e) {\n    cursor.style = \"transition:.15s; left:\".concat(e.clientX, \"px; top:\").concat(e.clientY, \"px\");\n  });\n\n  for (var i = 0; i < linkBtn.length; i++) {\n    linkBtn[i].addEventListener('mouseenter', cursorFun);\n    linkBtn[i].addEventListener('mouseleave', cursorFun);\n  }\n\n  function cursorFun(e) {\n    if (e.type == 'mouseenter') {\n      cursor.classList.add('active');\n    } else {\n      cursor.classList.remove('active');\n    }\n  } //circle event\n\n\n  var circle = document.querySelector('.circle-pattern');\n  window.addEventListener('scroll', function () {\n    circle.style.transform = 'translateY(' + this.scrollY + 'px)';\n  });\n});\n\n//# sourceURL=webpack://providershop-master/./src/js/script.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/script.js"]();
+/******/ 	
+/******/ })()
+;
